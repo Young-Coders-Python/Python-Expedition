1d4650fbb0003af4995d08ddd7cea077b8e2fbf3
TofaelKabir <tofaelkabir@gmail.com>
2024-07-28 23:32:12 -0400

    Python String Update

--- Lec 02 Python String/01 Python String Basic.py
+++ Lec 02 Python String/01 Python String Basic.py
@@ -5,16 +5,19 @@ print('Hi')
 name = "Mohammad"
 print(name)
 print(type(name))
+
 # To get the length of a string, we use the len() function.
 print(len(name))
 
+print("--------------------------------------")
 # String Concatenation: when combine 2 String
 # To concatenate, or combine, two strings - we can use the + operator.
-print("Hello!" + "Welcome to Python") # No white space created
+print("Hello!" + "Welcome to Python") # No white space created --> "Hello!Welcome to Python"
 
-# We can use coma to concatenate
+# We can use coma to concatenate which provide space between String
 print("Good", "Evening")
 
+print("--------------------------------------")
 firstName = "Mohammad"
 lastName = "Sharkar"
 
@@ -22,7 +25,8 @@ print(firstName + lastName)
 print(firstName + " " + lastName) # This is not smart code
 print(firstName, lastName) # we will use this
 
-# We can assign a multiline string to a variable by using three double or single quotes:
+print("--------------------------------------")
+# We can assign a multiline string to a variable by using three double or single quotes (not common)
 a ="""My Name is Alex, 
     I am 200 years old,
     I live in NY,
@@ -31,12 +35,8 @@ a ="""My Name is Alex,
 print(a)
 print(len(a))
 
-
-
-
 print("--------------------------------------")
-# Tofael's Book
-# Quotes Inside Quotes
+# Quotes Inside Quotes: Tofael's Book
 # We can use quotes inside a string, unless they don't match the quotes surrounding the string
 print("Tofael's Book")
 print('My Name is "Sharkar" and I am 100 years old')
